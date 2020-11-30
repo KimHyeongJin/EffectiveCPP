4d5c9628454a280313e429bb7a3816876e9aa6a5
KimHyeongJIn <hyeongjin78@gmail.com>
2020-11-30 10:06:15 +0900

    Auto-committed on 2020-11-30

--- Empty.h
+++ Empty.h
@@ -20,8 +20,34 @@ class Empty
 	}
 };
 
+//바이트 패딩 테스트 클래스
 class BytePadding
 {
 	char x;
 	int y;
 };
+
+//sizeof(HoldsAnInt) > sizeof(int)
+class HoldsAnInt	// int를 저장할 공간만 필요해야 합니다.
+{
+private:
+	int x;
+	Empty e;	// 메모리 요구가 없어야 합니다.
+};
+
+//sizeof(HoldsAnInt) == sizeof(int)
+class HoldsAnInt2 : private Empty
+{
+private:
+	int x;
+};
+/*
+sizeof(HoldsAnInt2) == sizeof(int)를 확인할 수 있습니다. 이 공간 절약
+기법은 공백 기본 클래스 최적화(Empty Base Opimization:EBO)라고 알려져 있으며,
+필자가 테스트한 모든 컴파일러에서 구현하고 있답니다. 메모리 공간에 많이 신경쓰는
+사용자를 상대하는 라이브러리 개발자라면 EBO를 알아두는 게 좋을 것입니다. 이와
+더불어 알아두면 쓸 만한 사실이 하나 더 있는데, EBO는 일반적으로 단일 상속에서만
+적용된다는 점입니다. C++ 객체의 레이아웃을 결정하는 규칙이 일반적으로 이러저러한
+관계로, 기본 클래스를 두 개 이상 갖는 파생 클래스에는 EBO가 적용될 수 없다고
+하네요.
+*/
